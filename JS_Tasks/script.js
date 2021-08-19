da0ddd8483542b2860fe4ec83410eb4ac514fd0b
CrUeLGeRoI <elbg2016@gmail.com>
2021-08-19 21:02:40 +0300

    Js tasks and lessons

---
+++ JS_Tasks/script.js
@@ -0,0 +1,56 @@
+// 1
+
+// let triangle = ''
+// for (let i = 0; i < 7; i++) {
+//     triangle += '#'
+//     console.log(triangle)
+// }
+
+// 2
+
+// let a = 0
+// const b = 'Fizz'
+// const c = 'Buzz'
+// for (i = 0; i < 100; i++){
+//     a++
+//     if (a % 3 == 0 && a % 5 == 0){
+//         console.log('FizzBuzz')
+//     }
+//     else{
+//         if (a % 3 == 0) {
+//             console.log(b)
+//         }
+//         else if (a % 5 == 0){
+//             console.log(c)
+//         }
+//         else{
+//             console.log(a)
+//         }
+//     }
+// }
+
+// 3
+
+//  4
+
+// function min(a, b) {
+//     if (a < b) {
+//         console.log(a)
+//     }
+//     else if (b < a){
+//         console.log(b)
+//     }
+//     else {
+//         console.log('Different numbers ONLY!')
+//     }
+// }
+
+// 5
+function isEven(a) {
+    let b = false
+    if (a % 2 == 0) {
+        b = true
+    }
+    console.log(b)
+    
+}
--- modal_window/index.js
+++ modal_window/index.js
@@ -2,5 +2,6 @@ const myModal = $.modal({
     title: 'Some Modal',
     closable: true,
     content: `<h4>BABABABABAABBALBALBA</h4>`,
-    width: '400px'
+    width: '400px',
+    
 })
