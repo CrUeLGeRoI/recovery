371e9cb4452db0d07b4d2e9f62a26930b72eb0ae
CrUeLGeRoI <elbg2016@gmail.com>
2021-08-24 19:15:07 +0300

    New exercises

--- JS_Tasks/script.js
+++ JS_Tasks/script.js
@@ -31,7 +31,7 @@
 
 // 3
 
-//  4
+// 4
 
 // function min(a, b) {
 //     if (a < b) {
@@ -46,11 +46,88 @@
 // }
 
 // 5
-function isEven(a) {
-    let b = false
-    if (a % 2 == 0) {
-        b = true
-    }
-    console.log(b)
+
+// function isEven(a) {
+//     let b = false
+//     if (a % 2 == 0) {
+//         b = true
+//     }
+//     console.log(b)
     
-}
+// }
+
+// 6
+
+// function countBs(str) {
+//     let b = str.length - 1
+//     console.log(b)
+// }
+// function countChar(str, a) {
+//     for (let i = 0; i <= str.length - 1; i++){
+//         console.log(str.charAt(i))
+//         if (str.charAt(i) == a){
+//             console.log("Success!")
+//         }
+//         else{
+//             console.log("Fail!")
+//         }
+//     }
+// }
+
+// 7
+// let c = []
+// function range(a, b, d) {
+//     //Not required param
+//     if (d == null){
+//         c = []
+//         for (let i = a; i <= b; i += 1){
+//             c.push(i)
+//         }
+//     }
+//     else{
+//         c = []
+//         if (d > 0){
+//             for (let i = a; i <= b; i += d){
+//                 c.push(i)
+                
+//             }
+//         }
+//         else if (d < 0){
+//             if (a < 0 || b < 0){
+//                 for (let i = a; i >= b; i += d){
+//                     c.push(i)
+//                 }
+//             }
+//             for (let i = b; i >= a; i += d){
+//                 c.push(i)
+//             }
+//         }
+//     }
+//     console.log(c)
+// }
+// function sum(){
+//     let b = 0
+//     for (let a in c){
+//         b += c[a]
+//         a++
+//     }
+//     console.log(b)
+// }
+
+// function range(a, b) {
+//     for (let i = a; i <= b; i++){
+//         c.push(i)
+        
+//     }
+//     console.log(c)
+// }
+// function sum(){
+//     let b = 0
+//     for (let a in c){
+//         b += c[a]
+//         a++
+//     }
+//     console.log(b)
+// }
+
+//8
